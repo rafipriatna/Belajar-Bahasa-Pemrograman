45bc47f19e09acb4a692d09da517a4d1ce2e3b36
Rafi <stormrider136@gmail.com>
2019-12-21 20:26:05 +0700

    Menambahkan perulangan do while

--- C/Struct UTS B.c
+++ C/Struct UTS B.c
@@ -42,99 +42,107 @@ void main(){
     // Array
     struct data_penjualan my_data[100];
 
-    // Input blocks
-    printf("Banyaknya data : ");scanf("%d", &jumlah_data);
-    getchar(); // Supaya fgets dapat digunakan
-
-    mulai: // Label untuk menandakan dimulainya program
-
-    // Perulangan sampai jumlah data
-    for (i = akhir; i <= jumlah_data; i++){
+    /*
+     * Membuat perulangan do while
+     * Supaya bisa mengulang programnya jika sudah selesai
+     */
+    do {
         /*
          *  gunakan system("cls"); jika kamu menggunakan OS Windows
          *  gunakan system("clear"); jika kamu menggunakan GNU / Linux
          */
         system("clear"); // Untuk membersihkan layar
-        printf("Data ke-%i", i);
-        printf("\nNama Pembeli  : ");fgets(my_data[i].nama, 20, stdin);
-        printf("Alamat          : ");fgets(my_data[i].alamat, 20, stdin);
-        printf("Tipe Rumah      : ");fgets(my_data[i].tipe, 20, stdin);
+        // Input blocks
+        printf("Banyaknya data : ");scanf("%d", &jumlah_data);
+        getchar(); // Supaya fgets dapat digunakan
 
-        /* Penjelasan tentang strcasecmp() ada di bawah :)
-         * Mencoba memberi pemberitahuan jika tipe rumah yang diinput tidak ada
-         * Rencanya mau pakai logika || (or), namun tidka berhasil
-         * AKhirnya, pakai if seperti ini
-         */
-        if (strcasecmp(my_data[i].tipe, "Mawar") != 10){
-            if (strcasecmp(my_data[i].tipe, "Melati") != 10){
-                if (strcasecmp(my_data[i].tipe, "Anggrek") != 10){
-                    if (strcasecmp(my_data[i].tipe, "Bougenvile") != 10){
-                        system("clear");
-                        printf("\nTipe rumah tidak ditemukan!");
-                        printf("\nIngin mengulangi lagi (y/n)? ");scanf("%c", &ulang);
-                        // Jika ulang = Y atau ulang = Y
-                        if (ulang == 'Y' || ulang == 'y'){
-                            system("clear"); // Bersihkan layar
-                            akhir = i; // Isi variabel akhir dengan perulangan for terakhir
-                            getchar(); // Supaya fgets dapat digunakan
-                            goto mulai; // Balik ke label mulai
-                            // Jika ulang = selain Y atau y
-                        }else{
-                            exit(main); // Exit dari fungsi main atau keluar dari program
-                        } // end if
+        mulai: // Label untuk menandakan dimulainya program
+
+        // Perulangan sampai jumlah data
+        for (i = akhir; i <= jumlah_data; i++){
+            system("clear"); // Untuk membersihkan layar
+            printf("Data ke-%i", i);
+            printf("\nNama Pembeli  : ");fgets(my_data[i].nama, 20, stdin);
+            printf("Alamat          : ");fgets(my_data[i].alamat, 20, stdin);
+            printf("Tipe Rumah      : ");fgets(my_data[i].tipe, 20, stdin);
+
+            /* Penjelasan tentang strcasecmp() ada di bawah :)
+            * Mencoba memberi pemberitahuan jika tipe rumah yang diinput tidak ada
+            * Rencanya mau pakai logika || (or), namun tidka berhasil
+            * AKhirnya, pakai if seperti ini
+            */
+            if (strcasecmp(my_data[i].tipe, "Mawar") != 10){
+                if (strcasecmp(my_data[i].tipe, "Melati") != 10){
+                    if (strcasecmp(my_data[i].tipe, "Anggrek") != 10){
+                        if (strcasecmp(my_data[i].tipe, "Bougenvile") != 10){
+                            system("clear");
+                            printf("\nTipe rumah tidak ditemukan!");
+                            printf("\nIngin mengulangi lagi (y/n)? ");scanf("%c", &ulang);
+                            // Jika ulang = Y atau ulang = Y
+                            if (ulang == 'Y' || ulang == 'y'){
+                                system("clear"); // Bersihkan layar
+                                akhir = i; // Isi variabel akhir dengan perulangan for terakhir
+                                getchar(); // Supaya fgets dapat digunakan
+                                goto mulai; // Balik ke label mulai
+                                // Jika ulang = selain Y atau y
+                            }else{
+                                exit(main); // Exit dari fungsi main atau keluar dari program
+                            } // end if
+                        }
                     }
                 }
             }
+
         }
 
-    }
-
-    // Output blocks
-    /*
-     *  gunakan system("cls"); jika kamu menggunakan OS Windows
-     *  gunakan system("clear"); jika kamu menggunakan GNU / Linux
-     */
+        // Output blocks
         system("clear"); // Untuk membersihkan layar
-    // Perulangan sampai jumlah data
-    for (i = 1; i <= jumlah_data; i++){
-        /* Menentukan harga rumah berasarkan tipe rumah yang diinput
-         *
-         * strcasecmp() adalah fungsi untuk membandingkan string1 dan string 2 dan tidak case sensitive
-         * akan mengembalikan menjadi 0 jika nilai ASCII string1 dan string2 itu sama
-         * akan mengembalikan menjadi negatif (-1, -2 dst...) jika nilai ASCII string1 < string2
-         * akan mengembalikan menjadi positif (1, 2 dst...) jika nilai ASCII string1 > string2
-         *
-         * Dalam hal ini, saya menggunakan 10 bukan 0 untuk membandingkan string
-         * karena saya menggunakan fgets, di mana fgets tidak menghapus newline pada string
-         * sehingga didapat return 10 bukan 0
-         *
-         */
+        // Perulangan sampai jumlah data
+        for (i = 1; i <= jumlah_data; i++){
+            /* Menentukan harga rumah berasarkan tipe rumah yang diinput
+            *
+            * strcasecmp() adalah fungsi untuk membandingkan string1 dan string 2 dan tidak case sensitive
+            * akan mengembalikan menjadi 0 jika nilai ASCII string1 dan string2 itu sama
+            * akan mengembalikan menjadi negatif (-1, -2 dst...) jika nilai ASCII string1 < string2
+            * akan mengembalikan menjadi positif (1, 2 dst...) jika nilai ASCII string1 > string2
+            *
+            * Dalam hal ini, saya menggunakan 10 bukan 0 untuk membandingkan string
+            * karena saya menggunakan fgets, di mana fgets tidak menghapus newline pada string
+            * sehingga didapat return 10 bukan 0
+            *
+            */
 
-        if (strcasecmp(my_data[i].tipe, "Mawar") == 10){
-            my_data[i].harga = 500;
-        }else if (strcasecmp(my_data[i].tipe, "Melati") == 10){
-            my_data[i].harga = 700;
-        }else if (strcasecmp(my_data[i].tipe, "Anggrek") == 10){
-            my_data[i].harga = 800;
-        }else if (strcasecmp(my_data[i].tipe, "Bougenvile") == 10){
-            my_data[i].harga = 1200;
+            if (strcasecmp(my_data[i].tipe, "Mawar") == 10){
+                my_data[i].harga = 500;
+            }else if (strcasecmp(my_data[i].tipe, "Melati") == 10){
+                my_data[i].harga = 700;
+            }else if (strcasecmp(my_data[i].tipe, "Anggrek") == 10){
+                my_data[i].harga = 800;
+            }else if (strcasecmp(my_data[i].tipe, "Bougenvile") == 10){
+                my_data[i].harga = 1200;
+            }
+
+            /*
+            * Menghitung pajak dan total.
+            * Pajak = 25% dari harga
+            * Total = Harga + pajak
+            */
+            my_data[i].pajak = 0.25 * my_data[i].harga;
+            my_data[i].total = my_data[i].harga + my_data[i].pajak;
+
+            // Tampilkan hasil perhitungan ke layar
+            printf("Data ke-%i", i);
+            printf("\nTipe Rumah   : %s", my_data[i].tipe);
+            printf("Harga        : %2.f", my_data[i].harga);
+            printf("\nPajak        : %2.f", my_data[i].pajak);
+            printf("\nTotal        : %2.f", my_data[i].total);
+            printf("\n\n");
         }
-
-        /*
-         * Menghitung pajak dan total.
-         * Pajak = 25% dari harga
-         * Total = Harga + pajak
-         */
-         my_data[i].pajak = 0.25 * my_data[i].harga;
-         my_data[i].total = my_data[i].harga + my_data[i].pajak;
-
-         // Tampilkan hasil perhitungan ke layar
-         printf("Data ke-%i", i);
-         printf("\nTipe Rumah   : %s", my_data[i].tipe);
-         printf("Harga        : %2.f", my_data[i].harga);
-         printf("\nPajak        : %2.f", my_data[i].pajak);
-         printf("\nTotal        : %2.f", my_data[i].total);
-         printf("\n\n");
-    }
-
+        printf("\nApakah kamu ingin mengulangi program (y/n)? ");scanf("%c", &ulang);
+    } // end do
+    /*
+     * Jika ulang = Y atau ulang = Y
+     * Maka, balik lagi ke awal do
+     */
+    while (ulang == 'y' || ulang == 'Y');
 }
